ef324ee852a2fd01d89a4e23453cec0d4b17f782
vlnarasimham143 <vlnarasimham@gmail.com>
2026-06-18 12:51:39 +0530

    Add SSS login screen app

---
+++ app/parent-dashboard/page.jsx
@@ -0,0 +1,9 @@
+import DashboardShell from "../DashboardShell";
+
+export const metadata = {
+  title: "Parent Dashboard | SSS Portal"
+};
+
+export default function ParentDashboard() {
+  return <DashboardShell role="Parent" />;
+}
